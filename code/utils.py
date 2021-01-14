c162361bb0b2884f5623744e8f8266e4baa712c1
hvgazula <hvgazula@umich.edu>
2021-01-13 23:24:46 -0500

    removed irrelevant functions

--- code/utils.py
+++ code/utils.py
@@ -1,40 +1,8 @@
 import glob
-import math
 import os
-import random
 
 import mat73
-import numpy as np
 import pandas as pd
-import torch
-
-
-def read_file(fh):
-    """Read file line-by-line and store in list
-
-    Args:
-        fh (string): name of the file
-
-    Returns:
-        list: contents of the file
-    """
-    with open(fh, 'r') as f:
-        lines = [line.rstrip() for line in f]
-    print(f'Number of Conversations is: {len(lines)}')
-    return lines
-
-
-def fix_random_seed(CONFIG):
-    """Fix random seed
-
-    Args:
-        CONFIG (dict): configuration information
-    """
-    SEED = CONFIG['seed']
-    random.seed(SEED)
-    np.random.seed(SEED)
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed_all(SEED)
 
 
 def extract_elec_ids(conversation):
@@ -120,42 +88,6 @@ def return_conversations(CONFIG):
     return convs
 
 
-def return_examples(file, delim, ex_words, vocab_str='std'):
-    """Parse conversations to return examples
-
-    Args:
-        file (str): conversation file
-        delim (str): text delimiter in the conversation file
-        ex_words (list): words to be excluded
-        vocab_str (str, optional): vocabulary to use. (Standard/SentencePiece)
-                                   defaults to 'std'.
-
-    Returns:
-        list: example tuples
-    """
-    with open(file, 'r') as fin:
-        lines = map(lambda x: x.split(delim), fin)
-        examples = map(
-            lambda x: (" ".join([
-                z for y in x[0:-4]
-                if (z := y.lower().strip().replace('"', '')) not in ex_words
-            ]), x[-1].strip(), x[-4], x[-3], x[-2]), lines)
-        examples = filter(lambda x: len(x[0]) > 0, examples)
-        if vocab_str == 'spm':
-            examples = map(
-                lambda x:
-                (vocabulary.EncodeAsIds(x[0]), x[1], int(float(x[2])),
-                 int(float(x[3])), int(float(x[4]))), examples)
-        elif vocab_str == 'std':
-            examples = map(
-                lambda x: (x[0].split(), x[1], int(float(x[2])),
-                           int(float(x[3])), int(float(x[4]))), examples)
-            examples = filter(lambda x: len(x[0]) == 1, examples)
-        else:
-            print("Bad vocabulary string")
-        return list(examples)
-
-
 def return_examples_new(file, delim, ex_words, vocab_str='std'):
     df = pd.read_csv(file,
                      sep=' ',
@@ -165,101 +97,6 @@ def return_examples_new(file, delim, ex_words, vocab_str='std'):
     df = df[~df['word'].isin(ex_words)]
 
     return df.values.tolist()
-
-
-def calculate_windows_params(CONFIG, gram, param_dict):
-    """Add offset to window begin, end and count bins
-
-    Args:
-        CONFIG (dict): configuration information
-        gram (tuple): tuple object of word and its corresponding signal window
-        param_dict (dict): signal parameters
-
-    Returns:
-        int: sequence length
-        int: index for window start
-        int: index for window end
-        int: number of bins in that window
-    """
-    seq_length = gram[3] - gram[2]
-    begin_window = gram[2] + param_dict['start_offset']
-    end_window = gram[3] + param_dict['end_offset']
-
-    if CONFIG["classify"] and CONFIG["ngrams"] and not CONFIG["nseq"]:
-        num_bins = 50
-    elif CONFIG["classify"] and not CONFIG["ngrams"] and not CONFIG[
-            "nseq"]:  # fixed number of bins
-        left_window = param_dict["left_window"]
-        right_window = param_dict["right_window"]
-        num_bins = len(range(-left_window, right_window, param_dict["bin_fs"]))
-    else:
-        num_bins = int(
-            math.ceil((end_window - begin_window) / param_dict['bin_fs']))
-
-    return seq_length, begin_window, end_window, num_bins
-
-
-def convert_ms_to_fs(CONFIG, fs=512):
-    """Convert seconds to frames
-
-    Args:
-        CONFIG (dict): Configuration information
-        fs (int, optional): Frames per second. Defaults to 512.
-
-    Returns:
-        dict: parameters for extracting neural signals
-    """
-    window_ms = CONFIG["window_size"]
-    if len(window_ms) == 1:
-        window_fs = int(window_ms[0] / 1000 * fs)
-        half_window = window_fs // 2
-        left_window = half_window
-        right_window = half_window
-    else:
-        left_window = int(window_ms[0] / 1000 * fs)
-        right_window = int(window_ms[1] / 1000 * fs)
-
-    shift_ms = CONFIG["shift"]
-    bin_ms = CONFIG["bin_size"]
-
-    bin_fs = int(bin_ms / 1000 * fs)
-    shift_fs = int(shift_ms / 1000 * fs)
-    start_offset = -left_window + shift_fs
-    end_offset = right_window + shift_fs
-
-    signal_param_dict = dict()
-    signal_param_dict['bin_fs'] = bin_fs
-    signal_param_dict['shift_fs'] = shift_fs
-    # signal_param_dict['window_fs'] = window_fs
-    signal_param_dict['left_window'] = left_window
-    signal_param_dict['right_window'] = right_window
-    signal_param_dict['start_offset'] = start_offset
-    signal_param_dict['end_offset'] = end_offset
-
-    return signal_param_dict
-
-
-def test_for_bad_window(start, stop, shape):
-    # if the window_begin is less than 0 or
-    # check if onset is within limits
-    # if the window_end is less than 0 or
-    # if the window_end is outside the signal
-    # if there are not enough frames in the window
-    return (start < 0 or start > shape[0] or stop < 0 or stop > shape[0]
-            or stop - start < 0)
-
-
-def print_cuda_usage(CONFIG):
-    """CUDA usage statistics
-
-    Args:
-        CONFIG (dict): configuration information
-    """
-    print('Memory Usage:')
-    for i in range(CONFIG["gpus"]):
-        max_alloc = round(torch.cuda.max_memory_allocated(i) / 1024**3, 1)
-        cached = round(torch.cuda.memory_cached(i) / 1024**3, 1)
-        print(f'GPU: {i} Allocated: {max_alloc}G Cached: {cached}G')
 
 
 def load_header(conversation_dir):
@@ -272,7 +109,7 @@ def load_header(conversation_dir):
     """
     misc_dir = os.path.join(conversation_dir, 'misc')
 
-    # TODO: Assuming name of mat file is the same conversation_dir
+    # TODO: Assuming name of mat file is the same as conversation_dir
     # TODO: write a condition to check this
     header_file = glob.glob(os.path.join(misc_dir, '*_header.mat'))[0]
     if not os.path.exists(header_file):
