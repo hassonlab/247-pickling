9154956ed6842acf290246d05278263a45521cf7
hvgazula <hvgazula@umich.edu>
2021-08-19 05:56:05 -0400

    orequested 1 gpu

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=03:00:00
 #SBATCH --mem=192GB
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH -o './logs/%A.out'
@@ -12,7 +12,7 @@ if [[ "$HOSTNAME" == *"tiger"* ]]
 then
     echo "It's tiger"
     module load anaconda
-    source activate torch-env
+    source activate 247-main
 elif [[ "$HOSTNAME" == *"della-gpu"* ]]
 then
     echo "It's della-gpu"
