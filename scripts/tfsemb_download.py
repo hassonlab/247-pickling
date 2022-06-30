a1f4200e65861005bc0929aa23ef8ca35d4b0bb1
hvgazula <hvgazula@users.noreply.github.com>
2022-06-30 01:24:51 -0400

    added list of models to use with git lfs
    check if caching was successful

--- scripts/tfsemb_download.py
+++ scripts/tfsemb_download.py
@@ -19,12 +19,26 @@ CAUSAL_MODELS = [
 ]
 SEQ2SEQ_MODELS = ["facebook/blenderbot_small-90M"]
 
+CLONE_MODELS = ["EleutherAI/gpt-neox-20b", "facebook/opt-6.7b", "facebook/opt-30b"]
+
 # TODO: Add MLM_MODELS (Masked Language Models)
 
 
 def download_tokenizer_and_model(
     CACHE_DIR, tokenizer_class, model_class, model_name, local_files_only
 ):
+    """Cache (or load) the model and tokenizer from the model repository (or cache).
+
+    Args:
+        CACHE_DIR (str): path where the model and tokenizer will be cached.
+        tokenizer_class (Tokenizer): Tokenizer class to be instantiated for the model.
+        model_class (Huggingface Model): Model class corresponding to model_name.
+        model_name (str):  Model name as seen on https://hugginface.co/models.
+        local_files_only (bool, optional): False (Default) if caching and True if loading from cache.
+
+    Returns:
+        tuple: (tokenizer, model)
+    """
     print("Downloading model")
     model = model_class.from_pretrained(
         model_name,
@@ -44,44 +58,71 @@ def download_tokenizer_and_model(
     return (model, tokenizer)
 
 
-def download_neox_model(
-    # NOTE: This is a special case for gpt-neox-20b for a shortwhile
+def clone_model_repo(
     CACHE_DIR,
     tokenizer_class,
     model_class,
     model_name,
     local_files_only=False,
 ):
+    """Cache (load) the model and tokenizer from the model repository (cache).
+
+    Args:
+        CACHE_DIR (str): path where the model and tokenizer will be cached.
+        tokenizer_class (Tokenizer): Tokenizer class to be instantiated for the model.
+        model_class (Huggingface Model): Model class corresponding to model_name.
+        model_name (str):  Model name as seen on https://hugginface.co/models.
+        local_files_only (bool, optional): False (Default) if caching and True if loading from cache.
+
+    Returns:
+        tuple or None: (tokenizer, model) if local_files_only is True
+                        None if local_files_only is False.
+    """
     model_dir = os.path.join(CACHE_DIR, model_name)
 
     if local_files_only:
         if os.path.exists(model_dir):
-            download_tokenizer_and_model(
+            model, tokenizer = download_tokenizer_and_model(
                 CACHE_DIR, tokenizer_class, model_class, model_dir, local_files_only
             )
+            return model, tokenizer
         else:
             print(f"Model directory {model_dir} does not exist")
-            exit(1)
     else:
         try:
-            if "tiger" in os.uname().nodename:
+            if (
+                "tiger" in os.uname().nodename
+            ):  # probably redundant, but just in case we are on tiger
                 os.system("module load git")
+
             os.system(f"cd {CACHE_DIR} && git lfs install")
-            os.system("git clone https://huggingface.co/EleutherAI/gpt-neox-20b")
+            os.system(f"git clone https://huggingface.co/{model_name}")
         except:
             # FIXME: Raise appropriate exception
             print("Possible git lfs version issues")
-    exit()
+    return
 
 
 def set_cache_dir():
     CACHE_DIR = os.path.join(os.path.dirname(os.getcwd()), ".cache")
+    assert (
+        CACHE_DIR == f"scratch/gpfs/{os.getlogin()}/.cache"
+    ), "Cache directory is not correct"
     os.makedirs(CACHE_DIR, exist_ok=True)
     return CACHE_DIR
 
 
-def download_tokenizers_and_models(model_name=None, local_files_only=False):
+def download_tokenizers_and_models(model_name=None, local_files_only=False, debug=True):
+    """This function downloads the tokenizer and model for the specified model name.
 
+    Args:
+        model_name (str, optional): Model name as seen on https://hugginface.co/models. Defaults to None.
+        local_files_only (bool, optional): False (Default) if caching and True if loading from cache.
+        debug (bool, optional): Check if caching was successful. Defaults to True.
+
+    Returns:
+        dict: Dictionary with model name as key and (tokenizer, model) as value.
+    """
     CACHE_DIR = set_cache_dir()
 
     if model_name is None:
@@ -100,24 +141,37 @@ def download_tokenizers_and_models(model_name=None, local_files_only=False):
 
     model_dict = {}
     for model_name in MODELS:
-        # Special case for gpt-neox-20b
-        if model_name == "EleutherAI/gpt-neox-20b":
-            download_neox_model(
-                CACHE_DIR, AutoTokenizer, model_class, model_name, local_files_only
-            )
-            continue
-
         print(f"Model Name: {model_name}")
-        model_dict[model_name] = download_tokenizer_and_model(
+
+        cache_function = (
+            clone_model_repo
+            if model_name in CLONE_MODELS
+            else download_tokenizer_and_model
+        )
+
+        model_dict[model_name] = cache_function(
             CACHE_DIR,
             AutoTokenizer,
             model_class,
             model_name,
-            local_files_only=local_files_only,
+            local_files_only,
         )
+
+        # check if caching was successful
+        if debug:
+            try:
+                cache_function(
+                    CACHE_DIR,
+                    AutoTokenizer,
+                    model_class,
+                    model_name,
+                    True,
+                )
+            except:
+                print(f"Caching of {model_name} failed")
 
     return model_dict
 
 
 if __name__ == "__main__":
-    download_tokenizers_and_models("causal", local_files_only=False)
+    download_tokenizers_and_models("causal", local_files_only=False, debug=True)
