873a8c87bbadf91ca034dac20323f4ec22ab7f95
Harsha <hvgazula@umich.edu>
2022-12-24 21:30:51 -0500

    resolved https://github.com/hassonlab/247-pickling/issues/136

--- scripts/tfsemb_LMBase.py
+++ scripts/tfsemb_LMBase.py
@@ -7,27 +7,6 @@ from utils import load_pickle, main_timer
 from utils import save_pickle as svpkl
 
 
-def clean_lm_model_name(item):
-    """Remove unnecessary parts from the language model name.
-
-    Args:
-        item (str/list): full model name from HF Hub
-
-    Returns:
-        (str/list): pretty model name
-
-    Example:
-        clean_lm_model_name(EleutherAI/gpt-neo-1.3B) == 'gpt-neo-1.3B'
-    """
-    if isinstance(item, str):
-        return item.split("/")[-1]
-
-    if isinstance(item, list):
-        return [clean_lm_model_name(i) for i in item]
-
-    print("Invalid input. Please check.")
-
-
 def add_vocab_columns(args, df, column=None):
     """Add columns to the dataframe indicating whether each word is in the
     vocabulary of the language models we're using.
@@ -40,15 +19,13 @@ def add_vocab_columns(args, df, column=None):
         *tfsemb_dwnld.MLM_MODELS,
     ]:
         try:
-            tokenizer = tfsemb_dwnld.download_hf_tokenizer(
-                model, local_files_only=True
-            )
+            tokenizer = tfsemb_dwnld.download_hf_tokenizer(model, local_files_only=True)
         except:
             tokenizer = tfsemb_dwnld.download_hf_tokenizer(
                 model, local_files_only=False
             )
 
-        key = clean_lm_model_name(model)
+        key = tfsemb_dwnld.clean_lm_model_name(model)
         print(f"Adding column: (token) in_{key}")
 
         try:
