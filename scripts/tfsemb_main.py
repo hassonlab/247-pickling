43b424c362f023c7f80934e46ddcce556bf81fac
Zaid Kokaja <zkokaja@users.noreply.github.com>
2023-01-31 15:45:27 -0500

    Revert "minor refactor"
    This reverts commit e3d4c4d5d787c454b99fffafea94b54677011714.

--- scripts/tfsemb_main.py
+++ scripts/tfsemb_main.py
@@ -621,16 +621,15 @@ def main():
     assert len(utterance_df) != 0, "Empty dataframe"
 
     # Select generation function based on model type
-    match args.embedding_type:
-        case "glove50":
-            generate_func = generate_glove_embeddings
-        case item if item in tfsemb_dwnld.CAUSAL_MODELS:
-            generate_func = generate_causal_embeddings
-        case item if item in tfsemb_dwnld.SEQ2SEQ_MODELS:
-            generate_func = generate_conversational_embeddings
-        case _:
-            print('Invalid embedding type: "{}"'.format(args.embedding_type))
-            exit()
+    if args.embedding_type == "glove50":
+        generate_func = generate_glove_embeddings
+    elif args.embedding_type in tfsemb_dwnld.CAUSAL_MODELS:
+        generate_func = generate_causal_embeddings
+    elif args.embedding_type in tfsemb_dwnld.SEQ2SEQ_MODELS:
+        generate_func = generate_conversational_embeddings
+    else:
+        print('Invalid embedding type: "{}"'.format(args.embedding_type))
+        return
 
     # Generate Embeddings
     embeddings = None
